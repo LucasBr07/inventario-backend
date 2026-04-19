278b3f5529732ac3e093ed4c2bd119ebfe512b00
Antonio Lucas <133284222+LucasBr07@users.noreply.github.com>
2026-04-19 16:27:22 -0300

    Add files via upload

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
     @Autowired
