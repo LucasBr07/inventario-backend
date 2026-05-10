e498d2a2253e964eca68cf0a496474f0e5fa4b00
Antonio Lucas <133284222+LucasBr07@users.noreply.github.com>
2026-05-10 17:16:06 -0300

    Add files via upload

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import com.example.demo.repository.UsuarioRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @CrossOrigin(origins = "*") // 🔥 libera acesso do frontend
 @RestController
@@ -24,5 +25,30 @@ public class UsuarioController {
     @PostMapping("/login")
     public Usuario login(@RequestBody Usuario user) {
         return repository.findByLoginAndSenha(user.getLogin(), user.getSenha());
+    }
+
+        // LISTAR TODOS
+    @GetMapping("/listar")
+    public List<Usuario> listar() {
+        return repository.findAll();
+    }
+
+    // BUSCAR POR ID
+    @GetMapping("/{id}")
+    public Usuario buscar(@PathVariable Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    // EDITAR
+    @PutMapping("/{id}")
+    public Usuario editar(@PathVariable Integer id, @RequestBody Usuario user) {
+        user.setUsuario_id(id);
+        return repository.save(user);
+    }
+
+    // EXCLUIR
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Integer id) {
+        repository.deleteById(id);
     }
 }
